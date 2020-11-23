65980f38788cb521757046db3f54721ee8a90650
Ghawsi <ghausy55@gmail.com>
2020-11-23 09:50:47 +0430

    password reset name added

--- resources/views/reset-password.blade.php
+++ resources/views/reset-password.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Wink. — Reset Password</title>
+    <title>Science4All — Reset Password</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Merriweather|Nunito:200,200i,300,300i,400,400i,600,600i,800,800i,900,900i" rel="stylesheet">
